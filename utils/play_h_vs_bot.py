ccb4f2aa369c4baf33de5636fe77e2555aee3df9
Matteo Vannini <matteo.vannini93@gmail.com>
2025-01-23 11:35:00 +0100

    modified play_h_vs_bot

--- utils/play_h_vs_bot.py
+++ utils/play_h_vs_bot.py
@@ -19,33 +19,27 @@ def play_h_vs_b(turn):
     positions = np.zeros(9)
     epsilon = 0
 
-    while True:
+    while check_triplets(positions) == False :
         print_board(positions)
         if turn == 1:
             action = act(turn, positions, epsilon)
             positions[action] = 1
-            turn = 2
-            if check_triplets(positions) == True:
-                print_board(positions)
-                print('player1 wins!')
-                positions = np.zeros(9)
-                break
-            if check_triplets(positions) == 'Tie':
-                print_board(positions)
-                print('Tie!')
-                positions = np.zeros(9)
-                break
-        if turn == 2:
+            turn = 2      
+        else:
             action = act(turn, positions, epsilon)
             positions[action] = 2
             turn = 1
-            if check_triplets(positions) == True:
-                print_board(positions)
-                print('player2 wins!')
-                positions = np.zeros(9)
-                break
-            if check_triplets(positions) == 'Tie':
-                print('Tie!')
-                positions = np.zeros(9)
-                break
+        
+    if check_triplets(positions) == True and turn == 1:
+        print_board(positions)
+        print('player1 wins!')
+        positions = np.zeros(9)
+    elif check_triplets(positions) == 'Tie':
+        print_board(positions)
+        print('Tie!')
+        positions = np.zeros(9)
+    else:
+        print_board(positions)
+        print('player2 wins!')
+        positions = np.zeros(9)
             
