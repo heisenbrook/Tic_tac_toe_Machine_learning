69236f2e24416713026b0f600ca527c60698caa8
Matteo Vannini <matteo.vannini93@gmail.com>
2025-01-28 17:04:08 +0100

    Modified Q_player

--- utils/Q_player.py
+++ utils/Q_player.py
@@ -4,9 +4,14 @@ import numpy as np
 def update_q_table(Q, state, action, reward, new_state):
     alpha = 0.7
     gamma = 0.01
-    old = Q[state][action]
-    next_max = max(Q[new_state])
-    Q[state][action] = (1 -alpha) * old + alpha * (reward + gamma * next_max)
+    state = str(state)
+    new_state = str(new_state)
+    q_values = Q.get(state, np.zeros(9))
+    next_q_values = Q.get(new_state, np.zeros(9))
+    max_next_q_value = max(next_q_values)
+    q_values[action] = (1 -alpha) * q_values[action] + alpha * (reward + gamma * max_next_q_value)
+
+    Q[state] = q_values
     
 def sel_e_greedy_action(Q, positions, epsilon):
     str_positions = str(positions)
@@ -17,8 +22,8 @@ def sel_e_greedy_action(Q, positions, epsilon):
     else:
         q_val = Q[str_positions]
         empty_q_val = [q_val[x[0]] for x in empty_pos]
-        max_q_val = max(empty_q_val)
-        max_q_ind = random.choice([i for i in range(len(empty_pos)) if empty_q_val[i] == max_q_val])
+        max_q_val = np.max(empty_q_val)
+        max_q_ind = random.choice(np.argwhere(empty_q_val == max_q_val))
         action = empty_pos[max_q_ind] 
         return action
 
